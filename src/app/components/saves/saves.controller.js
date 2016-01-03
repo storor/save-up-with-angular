e56b5e4b6d962cf0a7f0d9b9a6e8b4ca4f56d715
storor <sergiy.tolok@gmail.com>
2016-01-03 23:21:41 +0200

    Use n3 pie chart in round progress

--- src/app/components/saves/saves.controller.js
+++ src/app/components/saves/saves.controller.js
@@ -1,43 +1,59 @@
 export class SavesController{
-  constructor ($log, savesService, goalsService) {
+  constructor ($log, $q, savesService, goalsService) {
     'ngInject';
     
     this.savesService = savesService;
     this.goalsService = goalsService;
     this.$log = $log;
+    this.$q = $q;
     
     this.isReady = "ready";
     this.saves = [];
     this.goal = 0;
+    this.totalSaved = 0;
+    this.donutOptions = {
+      thickness: 5,
+      mode: 'gauge'
+    }
+    this.donutData = [];
     
+    this.init();
+  }
+  
+  init(){
     this.getGoal();
-    this.getSaves();
+    this.getSaves();    
   }
   
   getSaves(){
     this.savesService.getSaves().then((saves)=>{
       this.saves = saves;      
+      this.getTotalSaved();
+      this.getDonutData();
     });
   }
   
   getGoal(){
     this.goalsService.getGoal().then((goal)=>{
-      this.goal = goal.amount;  
+      this.goal = goal.amount;
+      this.donutOptions.total = this.goal;
     });
   }
   
-  totalSaved(){
+  getTotalSaved(){
     var total = 0;
     for(var save of this.saves){
       total += save.amount;
-    }
-    return total;
+    }    
+    this.totalSaved = total;
   }
   
-  percent(){
-    if(this.goal){
-      return ( 100 / this.goal ) * this.totalSaved();
-    }
-    return 0;
-  }
+  getDonutData(){
+    this.donutData = [{
+      label: 'Saved',
+      value: this.totalSaved,
+      color: '#5cb85c'
+    }];
+    this.$log.info(this.donutData);
+  }  
 }
--- src/app/index.module.js
+++ src/app/index.module.js
@@ -7,7 +7,7 @@ import { SavesController } from './components/saves/saves.controller';
 import { SavesService } from './shared/saves/saves.service';
 import { GoalsService } from './shared/goals/goals.service';
 import { NavbarDirective } from '../app/components/navbar/navbar.directive';
-angular.module('saveUp', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'saveUp.config', 'angular-svg-round-progress'])
+angular.module('saveUp', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'saveUp.config', 'n3-pie-chart'])
   .constant('moment', moment)
   .config(config)
   .config(mockConfig)
